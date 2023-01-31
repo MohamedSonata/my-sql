212ed2330f051091ae8ae21fbfd3b6f12673c2c9
MohamedSonata <sonata.1993@yahoo.com>
2023-01-31 18:14:47 +0300

    first commit

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,16 +1,17 @@
-module.exports = ({ env }) => ({   connection: {
-    client: "mysql",
-    connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env("DATABASE_PORT", 25060),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      ssl:{
-        ca:env('DATABASE_CA')
-      }
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
-    debug:false   
-    }, 
-   });
+  },
+});
