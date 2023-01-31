c2a0c416bb62f2cd829b20f1be5b70cfd8a6547a
MohamedSonata <sonata.1993@yahoo.com>
2023-01-31 22:06:04 +0300

    first commit

--- config/database.js
+++ config/database.js
@@ -6,9 +6,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
+      database: env('DATABASE_NAME', 'root'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'root'),
     
     }, insecureAuth: true,
 
